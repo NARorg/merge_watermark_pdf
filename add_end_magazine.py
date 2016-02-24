ebe9d4869efa390a5334e2cc109cb16a876b800b
Ryan G. Coleman <ryan.g.coleman@gmail.com>
2016-02-23 19:06:02 -0700

    ugh

--- add_end_magazine.py
+++ add_end_magazine.py
@@ -5,20 +5,18 @@ import sys
 
 output = PdfFileWriter()
 input = PdfFileReader(open(sys.argv[1], "rb"), strict=False)
-watermark = PdfFileReader(open("rdfooter.pdf", "rb"))
-lastPage = PdfFileReader(open("rd.pdf", "rb"))
+lastPage = PdfFileReader(open("./mag.pdf", "rb"))
 
 # print how many pages input1 has:
 #print("test.pdf has %d pages." % input.getNumPages())
-#print("watermark.pdf has %d pages." % watermark.getNumPages())
 
 for pageNum in xrange(input.getNumPages()):
   page = input.getPage(pageNum)
-  page.mergePage(watermark.getPage(0))
   output.addPage(page)
 
 output.addPage(lastPage.getPage(0))
 
-outputStream = file(sys.argv[2], "wb")
+outputStream = open(sys.argv[2], "wb")
+print sys.argv[2]
 output.write(outputStream)
 outputStream.close()
--- watermark_and_add_end_magazine.py
+++ watermark_and_add_end_magazine.py
@@ -4,9 +4,9 @@ from PyPDF2 import PdfFileWriter, PdfFileReader
 import sys
 
 output = PdfFileWriter()
-input = PdfFileReader(open(sys.argv[1], "rb"))
-watermark = PdfFileReader(open("rdfootermag.pdf", "rb"))
-lastPage = PdfFileReader(open("mag.pdf", "rb"))
+input = PdfFileReader(open(sys.argv[1], "rb"), strict=False)
+watermark = PdfFileReader(open("./rdfootermag.pdf", "rb"))
+lastPage = PdfFileReader(open("./mag.pdf", "rb"))
 
 # print how many pages input1 has:
 #print("test.pdf has %d pages." % input.getNumPages())
@@ -19,6 +19,7 @@ for pageNum in xrange(input.getNumPages()):
 
 output.addPage(lastPage.getPage(0))
 
-outputStream = file(sys.argv[2], "wb")
+outputStream = open(sys.argv[2], "wb")
+print sys.argv[2]
 output.write(outputStream)
 outputStream.close()
